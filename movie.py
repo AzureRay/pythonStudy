98491677eee39bd9b70cda7a5701868b4f5abf14
AzureRay <476468781@qq.com>
2016-12-20 14:11:35 +0800

    update

--- movie.py
+++ movie.py
@@ -1,3 +1,15 @@
+##########################################
+# Project : My Favorite Movies Website
+# Date Started: 12/20/2016
+# Date Completed: 12/20/2016
+# Submitted by: Azure Ray
+##########################################
+
+######################################## Movie File ####################################################
+# Description: This file creates the class Movie to allow for instances of this class to be used in the
+#              main.py file. Class Movie has three basic propertities now.
+########################################################################################################
+
 class Movie:
     def __init__(self, title, poster_image_url, trailer_youtube_url):
         self.title = title
